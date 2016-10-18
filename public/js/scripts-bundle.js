a27831c136ed8fc753676c55f927da096c85ad14
Kristi Walker <kristiwalker22@gmail.com>
2016-10-18 17:27:23 -0500

    built story nav for desktop

--- public/js/scripts-bundle.js
+++ public/js/scripts-bundle.js
@@ -1,2 +1,2 @@
-$(document).ready(function(){var e=new Date,t=e.getFullYear();$(".copyright").text(t)});
+$(document).ready(function(){$("#stories-btn").click(function(){$(".stories-nav-wrapper").fadeIn(600)}),$(".close").click(function(){$(".stories-nav-wrapper").fadeOut(600)});var e=new Date,t=e.getFullYear();$(".copyright").text(t)});
 //# sourceMappingURL=scripts-bundle.js.map
